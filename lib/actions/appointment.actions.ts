fbb616921e484797151fc28d151f3021126b9408
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-07-12 17:16:30 +0200

    admin page templating

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -5,8 +5,9 @@ import {
   DATABASE_ID,
   databases,
 } from "../appwrite.config";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { parseStringify } from "../utils";
+import { Appointment } from "./appwrite.types";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams
@@ -33,6 +34,37 @@ export const getAppointment = async (appointmentId: string) => {
       appointmentId
     );
     return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        if ((appointment.status = "scheduled")) {
+          acc.scheduledCount++;
+        } else if ((appointment.status = "pending")) {
+          acc.pendingCount++;
+        } else if ((appointment.status = "cancelled")) {
+          acc.cancelledCount++;
+        }
+        return acc;
+      }
+    );
   } catch (error) {
     console.log(error);
   }
